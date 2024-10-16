03362dba0f845d75e22280bcfb6cdbfc09da2e9a
tpm909 <ankitoperez909@gmail.com>
2024-10-16 13:51:45 -0300

    cosa

--- web/fotos/foto.js
+++ web/fotos/foto.js
@@ -1,11 +1,11 @@
 var cosa = [
     
-    {download_url:"./fotos/download.jpg",
-        desc: "foto 1"
+    {download_url:"./fotos/catena-zapata-estiba-reservada-2016-vino-catena-zapata-585579.png",
+        desc: "Catena Zapata ha identificado los lotes de mejor calidad dentro de los viñedos de la familia. El cultivo y cuidado manual de dichos lotes ha permitido identificar aquellas plantas que, en forma consistente, producen la mejor fruta. Estas plantas son marcadas en el viñedo con una cinta roja, que indica el cuidado especial que se debe prestar a las mismas."
     }
     ,
-    { download_url:"./fotos/descargasdaw.jpg"}
+    { download_url:"./fotos/descargasdaw.jpg",desc:"foto2"}
     ,
-    {download_url:"./fotos/ljhbzdl.jpg"}
+    {download_url:"./fotos/descargasdaw.jpg",desc:"foto3"}
     
 ]
